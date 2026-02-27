8a8aa9392c8a869cf9529c923c6d2e93d36c9c79
alexc83 <alex.cruce@gmail.com>
2026-02-27 05:58:10 -0600

    finished main product grid

---
+++ src/app/components/product-card/product-card.ts
@@ -0,0 +1,19 @@
+import {Component, input, output} from '@angular/core';
+import {Product} from '../../models/product';
+import {MatButton} from '@angular/material/button';
+import {MatIcon} from '@angular/material/icon';
+
+@Component({
+  selector: 'app-product-card',
+  imports: [
+    MatButton,
+    MatIcon
+  ],
+  templateUrl: './product-card.html',
+  styleUrl: './product-card.scss',
+})
+export class ProductCard {
+  product = input.required<Product>();
+
+  addToCartClicked = output<Product>();
+}
--- src/app/pages/products-grid/products-grid.ts
+++ src/app/pages/products-grid/products-grid.ts
@@ -1,10 +1,25 @@
 import {Component, computed, input, signal} from '@angular/core';
 import {Product} from '../../models/product';
 import products from "../../data/products.json"
+import {ProductCard} from '../../components/product-card/product-card';
+import {MatSidenav, MatSidenavContainer, MatSidenavContent} from '@angular/material/sidenav';
+import {MatListItem, MatListItemTitle, MatNavList} from '@angular/material/list';
+import {RouterLink} from '@angular/router';
+import {TitleCasePipe, UpperCasePipe} from '@angular/common';
 
 @Component({
   selector: 'app-products-grid',
-  imports: [],
+  imports: [
+    ProductCard,
+    MatSidenavContainer,
+    MatSidenavContent,
+    MatSidenav,
+    MatNavList,
+    MatListItem,
+    MatListItemTitle,
+    RouterLink,
+    TitleCasePipe
+  ],
   templateUrl: './products-grid.html',
   styleUrl: './products-grid.scss',
 })
@@ -14,7 +29,10 @@ export default class ProductsGrid {
 
   products = signal<Product[]>(products);
 
-  filteredProducts = computed(() =>
-    this.products().filter(p => p.category === this.category().toLowerCase()));
-  protected readonly parseInt = parseInt;
+  filteredProducts = computed(() => {
+    if (this.category() === "all") return this.products();
+    return this.products().filter(p => p.category.toLowerCase() === this.category().toLowerCase());
+  });
+
+  categories = signal<string[]>(["all", "electronics", "fashion", "fitness", "furniture", "home"])
 }
